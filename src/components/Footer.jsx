a54d3632c545b028eaae904822112ff752e29420
othmanealn <othm.alouane@gmail.com>
2025-07-25 12:00:09 +0100

    Correction du Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import './Footer.css';
 
 function Footer (){
+  return(
     <footer className="footer">
-        &copy; 2025  Espace ALOUANE - Tous droits réservés.
+       <p> &copy; 2025  Espace ALOUANE - Tous droits réservés.</p>
       </footer>
+  );
 }
 export default Footer;
